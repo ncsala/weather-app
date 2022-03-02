3d3a065690e103a085353b8ad1167be2c067e51a
Nicolas Caceres Sala <ncsala@gmail.com>
2022-03-02 01:35:05 -0300

    PropTypes is added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import CardTemp from './CardTemp'
+import CardTemp from "./CardTemp";
+import PropTypes from "prop-types";
 
 export default function Card({ max, min, name, img, onClose }) {
   // Se Corrobora que onClose sea una funcion, si lo es se ejecuta
@@ -11,9 +12,22 @@ export default function Card({ max, min, name, img, onClose }) {
     <div>
       <button onClick={handleOnClose}>X</button>
       <span>{name}</span>
-      <CardTemp label="Min" value={min}/>
-      <CardTemp label="Max" value={max}/>
-      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Icono del clima"></img>
+      <CardTemp label="Min" value={min} />
+      <CardTemp label="Max" value={max} />
+      <img
+        src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+        alt="Icono del clima"
+      ></img>
     </div>
   );
 }
+
+
+// Esto es una buena practica para ver los errores que hay con los tipos de datos q se estan esperando
+Card.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
+  name: PropTypes.string,
+  img: PropTypes.string,
+  onClose: PropTypes.func,
+};
