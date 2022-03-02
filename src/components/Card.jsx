c301fddd31e9b3067bffff3c27848ceebcde85dd
Nicolas Caceres Sala <ncsala@gmail.com>
2022-03-02 01:22:53 -0300

    Create component CardTemp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CardTemp from './CardTemp'
 
 export default function Card({ max, min, name, img, onClose }) {
   // Se Corrobora que onClose sea una funcion, si lo es se ejecuta
@@ -10,14 +11,8 @@ export default function Card({ max, min, name, img, onClose }) {
     <div>
       <button onClick={handleOnClose}>X</button>
       <span>{name}</span>
-      <div>
-        <label>Min</label>
-        <span>{min}</span>
-      </div>
-      <div>
-        <label>Max</label>
-        <span>{max}</span>
-      </div>
+      <CardTemp label="Min" value={min}/>
+      <CardTemp label="Max" value={max}/>
       <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Icono del clima"></img>
     </div>
   );
