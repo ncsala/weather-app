d13197fde514b1674c82acdb1b817f8da809b4ba
Nicolas Caceres Sala <ncsala@gmail.com>
2022-03-03 20:27:49 -0300

    Add fetch with API

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
 import React from "react";
-import Cards from "./components/Cards.jsx";
-import SearchBar from "./components/SearchBar.jsx";
-import data from "./data.js";
 import styles from "./App.module.css";
-
-const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
-// Para guardar el apiKey de forma segura
-// const apiKey = process.env.REACT_APP_APIKEY;
-
-function onSearch(ciudad) {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-    .then(r => r.json())
-    .then((recurso) => {
-      if(recurso.main !== undefined){
-        const ciudad = {
-          min: Math.round(recurso.main.temp_min),
-          max: Math.round(recurso.main.temp_max),
-          img: recurso.weather[0].icon,
-          id: recurso.id,
-          wind: recurso.wind.speed,
-          temp: recurso.main.temp,
-          name: recurso.name,
-          weather: recurso.weather[0].main,
-          clouds: recurso.clouds.all,
-          latitud: recurso.coord.lat,
-          longitud: recurso.coord.lon
-        };
-        // setCities(oldCities => [...oldCities, ciudad]);
-      } else {
-        alert("Ciudad no encontrada");
-      }
-    });
-
-  }
+import fetchCity from "./services/fetchCity";
+import CitiesPage from "./views/CitiesPage";
+import { Route } from "react-router-dom";
 
 function App() {
-  const [data, setData] = React.useState([])
+  const [cities, setCities] = React.useState([]);
+
+  function onSearch(city) {
+    if (cities.length > 6) {
+      alert("No se pueden agregar más ciudades");
+    } else {
+      fetchCity(city, setCities);
+    }
+  }
+
+  function handleOnClose(id) {
+    setCities((prevData) => prevData.filter((city) => city.id !== id));
+  }
 
   return (
     <div className={styles.app}>
       <div className={styles.bkg} />
       <div className={styles.container}>
-        <div>
-          <SearchBar onSearch={onSearch} />
-        </div>
-
-        <div className={styles.citiesContainer}>
-          <Cards cities={data} />
-        </div>
+        <Route path='/'>
+          <CitiesPage
+            cities={cities}
+            handleOnClose={handleOnClose}
+            onSearch={onSearch}
+          />
+        </Route>
       </div>
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,31 @@ import { IoAdd } from "react-icons/io5";
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar({ onSearch }) {
+  const [search, setSearch] = React.useState();
+
   function handleOnSearch() {
     if (typeof onSearch === "function") {
-      const input = document.getElementById("search-bar-input");
-      onSearch(input.value);
+      // const input = document.getElementById("search-bar-input");
+      // onSearch(input.value);
+      // input.value = "";
+      onSearch(search);
+      setSearch("");
     }
   }
 
   return (
     <div className={styles.searchBar}>
-      <input id="search-bar-input" placeholder="Busque una ciudad..." className={styles.input}></input>
+      <input
+        id="search-bar-input"
+        placeholder="Busque una ciudad..."
+        className={styles.input}
+        autoComplete="off"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') handleOnSearch();
+        }}
+      ></input>
       <button onClick={handleOnSearch} className={styles.button}>
         <IoAdd />
       </button>
